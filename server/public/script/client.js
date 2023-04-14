c87dc72ca2d9b3e2ca9758f6001c8b3e9c747a47
Ilyas Mohamed <zswaleh@ymail.com>
2023-04-14 18:55:26 -0500

    the post is working for both client and server

--- server/public/script/client.js
+++ server/public/script/client.js
@@ -4,7 +4,34 @@ $(document).ready( onReady );
 
 function onReady() { 
 console.log('DOM');
-$('#addTask-btn').on('click', addTask);
+$('#addTasks-btn').on('click', addTask);
+
+}
+
+function addTask() {
+    let tasks = { 
+      task: $('#tasks-input').val(),
+      status: $('#status-input').val()
+    }
+$.ajax({
+    method: 'POST',
+    url: '/tasks',
+    data: tasks
+  }).then(function(responsePost) {
+    console.log(responsePost);
+    renderAndFetchAllTasks();
+  })
+}
+
+function renderAndFetchAllTasks(event){
+    event.preventDefault();
+    
+$.ajax({
+    method: 'GET',
+    url: '/tasks',
+}).then(function(responseGet) {
+    console.log(responseGet);
+})
 
 }
 
--- server/routes/tasks.router.js
+++ server/routes/tasks.router.js
@@ -4,3 +4,28 @@ const tasksRouter = express.Router();
 const pool = require('../modules/pool.js');
 
 module.exports = tasksRouter;
+
+tasksRouter.post('/', (req, res) => {
+    console.log('POST /tasks');
+    console.log('here is the data we got mailed:', req.body);
+
+    let newTask = req.body.task;
+    let newStatus = req.body.status;
+
+    let sqlText = `
+      INSERT INTO "to-do-list"
+      ("tasks", "status")
+      VALUES
+      ($1, $2);
+      `;
+    let sqlValues = [newTask, newStatus]
+    pool.query(sqlText, sqlValues)
+    .then((dbRes) => {
+      console.log('New task was added!')
+      res.sendStatus(201);
+    })
+    .catch((dbErr) => {
+      console.log('POST /task error:', dbErr);
+      res.sendStatus(500);
+    })
+})

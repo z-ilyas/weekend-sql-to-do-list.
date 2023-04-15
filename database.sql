de4c485342e24ffa394632c75b3786534b35cb03
Ilyas Mohamed <zswaleh@ymail.com>
2023-04-14 20:11:56 -0500

    updated the final database.sql

--- database.sql
+++ database.sql
@@ -1,11 +1,12 @@
 CREATE TABLE "to-do-list" (
     "id" SERIAL PRIMARY KEY,
     "tasks" VARCHAR(100) NOT NULL,
-	"completed" VARCHAR(100) NOT NULL
+	"status" VARCHAR(100) NOT NULL
 );
 
 INSERT INTO "to-do-list"
-("tasks", "completed")
+("tasks", "status")
 VALUES
-('When will I finish this Task', 'Not Done huh?'),
-('Stop being lazy and do the Task', 'Not Done huh?');
+('When will I finish this Task', 'complete'),
+('I exist merely to be used as a test', 'pending'),
+('Stop being lazy and do the Task', 'pending');
